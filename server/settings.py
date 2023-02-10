7e39c7e851af93f815acaefe8c55ead0aa18c376
yshubham8419 <yshubham592@gmail.com>
2023-02-10 12:35:23 +0530

    admin site removed debug set to false

--- server/settings.py
+++ server/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fe1-!07osj-5m7p-v38=8xz#khzvq@9wc9&swig6ch8g4_^i=9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
--- server/urls.py
+++ server/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('register/', views.register),
     path('login/', views.api_login),
     path('set-profile/', views.set_profile),
